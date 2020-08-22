f8ce65a543d96a7b07f2b95ccee26395a79805c9
Hashy <59665246+copev313@users.noreply.github.com>
2020-08-22 17:28:14 -0400

    Added Component: ColorPicker Added potential for color selection.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <ColorPicker/>
     <Canvas/>
   </div>
 </template>
 
 <script>
 import Canvas from "./components/Canvas";
+import ColorPicker from "./components/ColorPicker";
 
 export default {
   name: "App",
   components: {
-    Canvas
+    Canvas,
+    ColorPicker
   }
 };
 </script>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Pixel color="white"/>
+    <Pixel color="lightblue"/>
+    <Pixel color="blue"/>
+    <Pixel color="darkblue"/>
+  </div>
+</template>
+
+<script>
+import Pixel from "./Pixel";
+
+export default {
+  name: "ColorPicker",
+  components: {
+    Pixel
+  }
+};
+</script>
+
+<style scoped>
+</style>
